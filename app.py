5ea5338d3402cf302054b2880d5cb3fa518232c0
Saira Patel <sairapatel1@outlook.com>
2024-07-16 23:15:07 +0100

    Added division form and worksheet. Fully functioning and styled.

--- app.py
+++ app.py
@@ -7,10 +7,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def timesTableForm():
-    return render_template("index.html")
+    return render_template("timesTablesForm.html")
 
-@app.route("/worksheet", methods=["post", "get"])
-def worksheet():
+@app.route("/divisionForm")
+def divisionForm():
+    return render_template("divisionForm.html")
+
+
+@app.route("/timesTablesWorksheet", methods=["post", "get"])
+def timesTablesWorksheet():
     if request.method == "POST":
         # get worksheet title, instrs and num of questions
         title = request.form.get("title")
@@ -42,6 +47,45 @@ def worksheet():
             
         return render_template("worksheet.html", ws=Worksheet(title, instrs, questions))
     return "get"
+
+
+@app.route("/divisionWorksheet", methods=["post", "get"])
+def divisionWorksheet():
+    if request.method == "POST":
+        # get worksheet title, instrs and num of questions
+        title = request.form.get("title")
+        qCount = int(request.form.get("questions"))
+        instrs = request.form.get("instructions")
+
+        # get divisors 
+        selectedNums = request.form.getlist("times-tables")
+        upTo = int(request.form.get("up-to"))
+        
+
+        # generate every possible question using the numbers given
+        possibleQs = []
+        for i in range(2, upTo +1):
+            for j in selectedNums:
+                possibleQs.append(f"{i*int(j)} / {j}")
+
+        questions=[]
+        # randomly pick questions until enough have been added
+        while len(questions) < qCount:
+            chosen = random.choice(possibleQs)
+            possibleQs.remove(chosen)
+            questions.append(chosen)
+
+            # if all possible questions have been added, start adding duplicates
+            if len(possibleQs) == 0:
+                possibleQs = list(questions)
+
+            
+        return render_template("worksheet.html", ws=Worksheet(title, instrs, questions))
+    return "get"
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
